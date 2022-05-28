7bcef5d0f095ccf024ede6bbb25afa06e752bad6
DashaK1 <kungurovadasha@gmail.com>
2022-05-27 19:25:53 -0500

    Add new tests

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,14 +1,14 @@
-import org.checkerframework.checker.units.qual.g;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -255,7 +255,7 @@ public class FirstSeleniumTest {
 //    }
 //
 
-//    @Test
+    //    @Test
 //    public void genderFieldFemaleTest() {
 //        WebDriver driver = new ChromeDriver();
 //        driver.get("https://www.facebook.com/");
@@ -281,18 +281,63 @@ public class FirstSeleniumTest {
 //        maleButton.click();
 //    }
 //
-//    @Test
-//    public void customFieldMaleTest() {
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://www.facebook.com/");
-//        driver.findElement(By.xpath("//*[text()='Create new account']"));
-//        WebElement createNewAccButton = driver.findElement(By.xpath("//*[text()='Create new account']"));
-//        assertNotNull(createNewAccButton);
-//        createNewAccButton.click();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        WebElement customButton = driver.findElement(By.xpath("//label[text()='Custom']"));
-//        customButton.click();
-//    }
+    @Test
+    public void buttonFemaleTest() {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//input[@data-type='text']")));
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        driver.findElement(By.xpath("//*[@name='websubmit']")).click();
+        WebElement femaleButton = driver.findElement(By.xpath("//*[text() = 'Female']//following-sibling::*[@type='radio']"));
+        femaleButton.click();
+    }
+
+    @Test
+    public void buttonMaleTest() {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//input[@data-type='text']")));
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        driver.findElement(By.xpath("//*[@name='websubmit']")).click();
+        WebElement MaleButton = driver.findElement(By.xpath("//*[text() = 'Male']//following-sibling::*[@type='radio']"));
+        MaleButton.click();
+    }
+
+    @Test
+    public void buttonCustomTest() {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//input[@data-type='text']")));
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        driver.findElement(By.xpath("//*[@name='websubmit']")).click();
+        WebElement CustomButton = driver.findElement(By.xpath("//*[text() = 'Custom']//following-sibling::*[@type='radio']"));
+        CustomButton.click();
+    }
+
+    @Test
+    public void testTermLinkTest() {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//input[@data-type='text']")));
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        driver.findElement(By.id("terms-link")).click();
+        for(String str : driver.getWindowHandles()){
+            driver.switchTo().window(str);
+        }
+        driver.getCurrentUrl();
+    }
+    @Test
+    public void dataPolicyLinkTest() {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//input[@data-type='text']")));
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        driver.findElement(By.id("privacy-link")).click();
+        for(String str : driver.getWindowHandles()){
+            driver.switchTo().window(str);
+        }
+        driver.getCurrentUrl();
+    }
 
     @Test
     public void signUpTest() {
@@ -304,19 +349,15 @@ public class FirstSeleniumTest {
     public void gendersTest() throws InterruptedException {
         String femaleXpath = "//*[@name='sex' and @value=1]";
         String maleXpath = "//*[@name='sex' and @value=2]";
-
         driver.findElement(By.xpath("//*[text()='Create new account']")).click();
         assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
-
         Thread.sleep(1000);
-
         //Verify female gender is checked
         WebElement femaleButton = driver.findElement(By.xpath(femaleXpath));
         femaleButton.click();
         String isFemaleChecked = driver.findElement(By.xpath(femaleXpath)).getAttribute("checked");
         assertNotNull(isFemaleChecked);
         assertEquals("true", isFemaleChecked);
-
         //Verify male gender is checked
         WebElement maleButton = driver.findElement(By.xpath(maleXpath));
         maleButton.click();
@@ -327,7 +368,7 @@ public class FirstSeleniumTest {
     }
 
     @Test
-    public void errorMessageTest() throws InterruptedException {
+    public void errorMessageTestMobNumEmail() throws InterruptedException {
         driver.findElement(By.xpath("//*[text()='Create new account']")).click();
         assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
         Thread.sleep(1000);
@@ -339,25 +380,96 @@ public class FirstSeleniumTest {
     }
 
     @Test
+    public void errorMessageTestFirstName() {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//input[@data-type='text']")));
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        driver.findElement(By.xpath("//*[@name='websubmit']")).click();
+        driver.findElement(By.xpath("//*[@aria-label='First name']")).click();
+        WebElement error = driver.findElement(By.xpath("//*[contains(text(),'your name')]"));
+        assertNotNull(error);
+    }
+
+    @Test
+    public void errorMessageTestLastName() {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//input[@data-type='text']")));
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        driver.findElement(By.xpath("//*[@name='websubmit']")).click();
+        driver.findElement(By.xpath("//*[@aria-label='Last name']")).click();
+        WebElement error = driver.findElement(By.xpath("//*[contains(text(),'your name')]"));
+        assertNotNull(error);
+    }
+
+    @Test
+    public void errorMessageTestNewPassword() {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//input[@data-type='text']")));
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        driver.findElement(By.xpath("//*[@name='websubmit']")).click();
+        driver.findElement(By.xpath("//*[@aria-label='New password']")).click();
+        WebElement error = driver.findElement(By.xpath("//*[contains(text(),'Enter a combination')]"));
+        assertNotNull(error);
+    }
+
+    @Test
+    public void monthDropListTest() {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//input[@data-type='text']")));
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
+        driver.findElement(By.xpath("//*[@name='websubmit']")).click();
+        driver.findElement(By.xpath("//*[@title='Month']")).click();
+        driver.findElement(By.xpath("//*[text()='May']")).click();
+    }
+
+
+    @Test
     public void yearTest() throws InterruptedException {
         driver.findElement(By.xpath("//*[text()='Create new account']")).click();
         assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@title='Year']")).click();
         driver.findElement(By.xpath("//*[text()='1990']")).click();
-       String yearValue =  driver.findElement(By.xpath("//*[@title='Year']")).getAttribute("value");
-       assertEquals("1990",yearValue);
+        String yearValue = driver.findElement(By.xpath("//*[@title='Year']")).getAttribute("value");
+        assertEquals("1990", yearValue);
 
     }
+
     @ParameterizedTest
-    @ValueSource(strings = {"1905", "1950","2022"})
+    @ValueSource(strings = {"1905", "1950", "2020"})
     public void yearTestParametrized(String yearInput) throws InterruptedException {
         driver.findElement(By.xpath("//*[text()='Create new account']")).click();
         assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@title='Year']")).click();
-        driver.findElement(By.xpath("//*[text()='"+yearInput+"']")).click();
-        String yearValue =  driver.findElement(By.xpath("//*[@title='Year']")).getAttribute("value");
-        assertEquals(yearInput,yearValue);
+        driver.findElement(By.xpath("//*[text()='" + yearInput + "']")).click();
+        String yearValue = driver.findElement(By.xpath("//*[@title='Year']")).getAttribute("value");
+        assertEquals(yearInput, yearValue);
     }
+
+    @Test
+    public void actionTest() {
+        driver.get("https://daviktapes.com/");
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//a[text()='Company']")));
+//        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//a[text()='Company2']")));
+        WebElement element = driver.findElement(By.xpath("//a[text()='Company']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).build().perform();
+    }
+
+    @Test
+    public void waitAndClickTest() {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='Company']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='Company']"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[text()='Company']"))).click();
+
+
+    }
+
 }
